0259a2f039f759b51a99b8199fb45ac40388031b
yunus <yunus@inoutmail.ru>
2024-03-23 11:59:42 +0300

    Highlight for neogit

--- custom/highlights.lua
+++ custom/highlights.lua
@@ -10,35 +10,27 @@ M.override = {
 		italic = true,
 	},
 
-	DiffAdd = {
-		fg = "NONE",
-		bg = "#004400",
-	},
-	DiffText = {
-		fg = "#000000",
-		bg = "#ffa500",
-	},
-	DiffChange = {
-		fg = "NONE",
-		bg = "#454545",
-	},
-	DiffDelete = {
-		fg = "NONE",
-		bg = "#660000",
-	},
-	DiffRemoved = {
-		fg = "NONE",
-		bg = "#660000",
-	},
-	FloatBorder = {
-		fg = "#454545",
-		bg = "NONE",
-	},
+	DiffAdd = { fg = "NONE", bg = "#004400" },
+	DiffText = { fg = "#000000", bg = "#ffa500" },
+	DiffChange = { fg = "NONE", bg = "#454545" },
+	DiffDelete = { fg = "NONE", bg = "#660000" },
+	DiffRemoved = { fg = "NONE", bg = "#660000" },
+	FloatBorder = { fg = "#454545", bg = "NONE" },
 }
 
 ---@type HLTable
 M.add = {
 	NvimTreeOpenedFolderName = { fg = "green", bold = true },
+
+	NeogitHunkHeader = { fg = "white", bg = "black" },
+	NeogitDiffAdd = { fg = "NONE", bg = "#004400" },
+	NeogitDiffDelete = { fg = "NONE", bg = "#660000" },
+	NeogitDiffHeader = { fg = "white", bg = "black" },
+
+	NeogitHunkHeaderHighlight = { fg = "white", bg = "black" },
+	NeogitDiffAddHighlight = { fg = "NONE", bg = "#004400" },
+	NeogitDiffDeleteHighlight = { fg = "NONE", bg = "#660000" },
+	NeogitDiffHeaderHighlight = { fg = "white", bg = "black" },
 }
 
 return M
